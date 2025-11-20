74adbfadf043bcd786299b65f47b541a66901d95
Kincade Morimoto <kincadem@hawaii.edu>
2025-11-19 21:20:46 -1000

    Issue-13 Created a lifestyle survey

---
+++ src/app/lifestyle-survey/page.tsx
@@ -0,0 +1,18 @@
+import LifestyleSurvey from '@/components/LifestyleSurvey';
+
+/**
+ * Lifestyle Survey Demo Page
+ * 
+ * This page demonstrates the LifestyleSurvey component.
+ * Access it at: /lifestyle-survey
+ * 
+ * This is a standalone demonstration that doesn't require authentication
+ * and can be used for testing and development purposes.
+ */
+export default function LifestyleSurveyPage() {
+  return (
+    <div>
+      <LifestyleSurvey />
+    </div>
+  );
+}
